d75c50b9f2f180e60daba5356e3c2f4c4a5d3f32
Harrison Fenton-Fearn <harryff007@gmail.com>
2021-02-25 11:40:04 +0000

    Added the middle name to the database

--- src/main/java/com/targetis/gradrole/domain/User.java
+++ src/main/java/com/targetis/gradrole/domain/User.java
@@ -49,6 +49,10 @@ public class User extends AbstractAuditingEntity implements Serializable {
     private String firstName;
 
     @Size(max = 50)
+    @Column(name = "middle_name", length = 50)
+    private String middleName;
+
+    @Size(max = 50)
     @Column(name = "last_name", length = 50)
     private String lastName;
 
